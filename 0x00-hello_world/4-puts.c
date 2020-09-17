44a64b50c54178ed59aaa02927e88bfe7f0f5ab3
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-17 04:32:32 +0000

    Fix function documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - main function
+ * Desciption: prints a string
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
