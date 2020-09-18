8f8ee32f026c564a51f70c06686cd6443dedcf28
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-18 05:01:09 +0000

    Add C program to print numbers from 0 to 9 with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - main function
+ *
+ * Description: prints if a number is positive, negative or zero
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+
+	putchar('\n');
+	/* your code goes there */
+	return (0);
+}
