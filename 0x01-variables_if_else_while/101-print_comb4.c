c2dc3f39b41f544167056e0f5b9daec3f68271ba
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-19 00:46:55 +0000

    Add C program to print possible combinations of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,8 +22,8 @@ int main(void)
 				putchar(i + '0');
 				putchar(j + '0');
 				putchar(k + '0');
-
-				if (i != 7 || j != 8 || k != 9)
+				
+				if (i != 9 || j != 8 || k != 9)
 				{
 					putchar(',');
 					putchar(' ');
