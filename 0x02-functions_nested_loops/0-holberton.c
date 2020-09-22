8406d1778135fb64b250c0e4fd2013e1c91719cd
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-22 01:00:33 +0000

    Add C program to print 'Holberton' with _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int _putchar(char);
+
+/**
+ * main - main function
+ *
+ * Return: On success 1.
+ */
+
+int main(void)
+{
+
+	char name[10] = "Holberton\n";
+	int i = 0;
+
+	while(i < 9)
+	{
+		_putchar(name[i]);
+		i++;
+	}
+
+	return (0);
+}
