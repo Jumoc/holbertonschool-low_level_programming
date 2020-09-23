273adecda5dbd638c3dc77df082ebc8436df7aaa
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-23 04:12:01 +0000

    Fix missing function documentation

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,10 @@
 #include "holberton.h"
 
 /**
- * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ * _islower - checks if a character is lowercase
+ * @c: character to be checked
+ *
+ * Return: 1 if lowercase 0 otherwise
  */
 
 int _islower(int c)
@@ -10,8 +13,6 @@ int _islower(int c)
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
-	} else
-	{
-		return (0);
 	}
+	return (0);
 }
