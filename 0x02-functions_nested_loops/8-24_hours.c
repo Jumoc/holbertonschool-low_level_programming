fee5caa2a0751ea5aa69a1b0ff7ec29dad391707
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-23 23:23:58 +0000

    Add C program to print the 24 hours of a day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints 24 hours
+ */
+
+void jack_bauer(void)
+{
+	int i = 0;
+
+	while(i < 24)
+	{
+		int j = 0;
+
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+
+			j++;
+		}
+		i++;
+	}
+
+}
