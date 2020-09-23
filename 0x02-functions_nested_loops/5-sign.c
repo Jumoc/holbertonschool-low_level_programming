ea501c646f81c4ea7a58883658361d49d7e6b513
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-23 04:34:23 +0000

    Add C program that checks for a number state

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_sign - checks if a number is positive, negative or zero
+ * @n: number to be checked
+ *
+ * Return: 1 if positive -1 if negative and 0 if zero
+ */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
