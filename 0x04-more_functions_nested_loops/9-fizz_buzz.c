f7895f7d58f27d90ed54a0975cd2efcedf143caa
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-24 19:06:04 +0000

    Add C program to the fizz buzz problem

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("Fizz Buzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
