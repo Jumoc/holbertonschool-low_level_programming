e906c46ce704caab8d44969b2279c94205ae57c2
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-24 18:45:41 +0000

    Add C program to print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ *
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (!(size <= 0))
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	} else
+	{
+		_putchar('\n');
+	}
+}
