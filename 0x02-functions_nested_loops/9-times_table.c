f76e5e7370076c093aaeac8328bd71bdf5d911c9
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-24 02:57:13 +0000

    Fix printing was different in the example

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,34 +6,34 @@
 
 void times_table(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		int j;
-
 		for (j = 0; j < 10; j++)
 		{
 			int operation = (i * j);
 
 			if (operation < 10)
 			{
-				_putchar(operation + '0');
-				if (j != 9)
+				if (j != 0)
 				{
-				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				}
+				_putchar(operation + '0');
+				if (j != 9)
+				_putchar(',');
 			} else
 			{
+				if (j != 0)
+				{
+					_putchar(' ');
+				}
 				_putchar((operation / 10) + '0');
 				_putchar((operation % 10) + '0');
 				if (j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(',');
 			}
 		}
 		_putchar('\n');
