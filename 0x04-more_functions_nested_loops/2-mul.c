0eedd79782467a4b8654fa144ac5e953ba60cf18
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-09-24 16:37:39 +0000

    Add C program that multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number to be multiplied
+ * @b: second number to be multiplied
+ *
+ * Return: result of the operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
