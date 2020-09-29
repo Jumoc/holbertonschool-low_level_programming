a255374fd53e67a39359be84e9d62d14dc3389fc
Jumoc <juanpcaicedo2@gmail.com>
2020-09-29 00:10:57 -0500

    Fix missing betty check

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 
 /**
- * swap_int - swaps the value of two integers
+ * _puts - swaps the value of two integers
  *
- * @s: string to be counted
+ * @str: string to be counted
  */
 
 void _puts(char *str)
-{	
+{
 	int i = 0;
-	
-	while(str[i] != '\0')
+
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
