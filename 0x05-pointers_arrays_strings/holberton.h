182597f15dee83a2b0e83b57316412a1d16b1cac
Jumo <juanpcaicedo2@gmail.com>
2020-09-29 23:49:57 -0500

    Add header guard

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -7,3 +10,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+
+#endif /* HOLBERTON_H */
