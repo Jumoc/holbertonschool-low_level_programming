9ae5cd28ceed45ec8cdfe40db94c4a2f40c9d981
Jumoc <juanpcaicedo2@gmail.com>
2020-10-01 18:13:27 -0500

    Add C program to concat to strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenates two strings
+ *
+ * @dest: string that gets concatenated src
+ * @src: string to be concatenated
+ *
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *first = dest;
+       
+	while(*dest != '\0')
+	{
+		dest++;
+	}
+
+	for ( ; *src != '\0'; src++)
+	{
+		*dest = *src;
+	}
+
+	return (first);
+
+}
