b7e25a8e5a77a926c814452c901cec34124b595e
Jumoc <juanpcaicedo2@gmail.com>
2020-10-01 23:20:53 -0500

    Fix betty error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,12 @@
 #include "holberton.h"
 
 /**
- * *_strncpy - copies N bytes of a string into another
+ * *_strcmp - compares two strings
  *
- * @dest: string that gets concatenated src
- * @src: string to be concatenated
- * @n: number of bytes to be copied
+ * @s1: string that gets concatenated src
+ * @s2: string to be concatenated
  *
- * Return: concatenated string
+ * Return: 0 if equal, positive if s2 > s1 negative otherwise
  */
 
 int _strcmp(char *s1, char *s2)
@@ -20,7 +19,7 @@ int _strcmp(char *s1, char *s2)
 	while (s2[lonS2] != '\0')
 		lonS2++;
 
-	while(*s1 != '\0' || *s2 != '\0')
+	while (*s1 != '\0' || *s2 != '\0')
 	{
 		if (*s1 != *s2)
 		{
