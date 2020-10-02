02765b3a21a28bd91323220744e513fe8614fe22
Jumoc <juanpcaicedo2@gmail.com>
2020-10-02 02:28:04 -0500

    Add C program to capitalize words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - capitalizes words of a string
+ *
+ * @s: string to be capitalized
+ *
+ * Return: the proccesed string
+ */
+
+char *cap_string(char *s)
+{
+	int size = 0, i = 0;
+
+	while (s[size] != '\0')
+		size++;
+
+	while (s[i] != '\0')
+	{
+		if (i < size)
+		{
+			if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'
+			   || s[i] == ',' || s[i] == ';' || s[i] == '.'
+			   || s[i] == '!' || s[i] == '?' || s[i] == '\"'
+			   || s[i] == '(' || s[i] == ')' || s[i] == '{'
+			   || s[i] == '}')
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] = s[i + 1] - ('a' - 'A');
+				}
+			}
+		}
+		i++;
+	}
+
+	return (s);
+
+}
