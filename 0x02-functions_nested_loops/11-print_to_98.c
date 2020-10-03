aa616a33a4611f28ab28c5028ad93dd8efa9ff4c
Jumoc <juanpcaicedo2@gmail.com>
2020-10-02 19:42:42 -0500

    Add C program that prints all numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all numbers from n to 98
+ *
+ * @n: starting number
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	int i = n;
+
+	while (i != 98)
+	{
+		if (i == n)
+			printf("%d", i);
+
+		if (i != 98)
+			printf(", ");
+
+		if (i > 98)
+			i--;
+		else
+			i++;
+
+		printf("%d", i);
+	}
+
+	printf("\n");
+}
