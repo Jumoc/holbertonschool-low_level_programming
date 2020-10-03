c032c9f4a55aaccd140177143dec9eeb26ca965a
Jumoc <juanpcaicedo2@gmail.com>
2020-10-02 19:27:44 -0500

    Add C program that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - adds two numbers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of both numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
