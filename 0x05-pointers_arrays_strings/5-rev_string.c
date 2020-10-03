ca56f043ea10e187fb71b569bc48734084616066
Jumoc <juanpcaicedo2@gmail.com>
2020-10-03 11:15:37 -0500

    Add C program to reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * rev string - returns a string backwards
+ *
+ * @s: string to be converted
+ */
+
+void rev_string(char *s)
+{
+	int size = 0, aux, i;
+	char reversed[20];
+
+	while (s[size] != '\0')
+	{
+		size++;
+	}
+
+	aux = size - 1;
+	i = 0;
+
+	while (aux >= 0)
+	{
+		reversed[i] = s[aux];
+		i++;
+		aux--;
+	}
+    
+	reversed[i] = '\0';
+
+	i = 0;	
+
+	while (s[i] != '\0')
+	{
+		s[i] = reversed[i];
+		i++;
+	}
+}
