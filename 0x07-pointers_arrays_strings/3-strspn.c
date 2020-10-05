1d5a69a4e56d247b9fd595848d5d47d39e38526c
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-10-05 23:36:04 +0000

    Add C program that gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * *_strspn - locates a character in a string
+ *
+ * @s: string to look into
+ * @accept: character to look for
+ *
+ * Return: counter of ocurrences
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i = 0, j = 0, count = 0, acount = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		acount = count;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if(s[i] == accept[j])
+			{
+				count++;
+			}
+		}
+
+		if (count == acount)
+		{
+			return(count);
+		}
+	}
+
+	return (count);
+}
