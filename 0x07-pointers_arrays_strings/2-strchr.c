f1429bacc8c9189ebb8276fbc211b6bd447f59fc
Jumoc <juanpcaicedo2@gmail.com>
2020-10-05 15:04:14 -0500

    Add C program to return a pointer to the fisrt character found in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - locates a character in a string
+ *
+ * @s: string to look into
+ * @c: character to look for
+ *
+ * Return: the fitst occurrence of c in s
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (s);
+}
