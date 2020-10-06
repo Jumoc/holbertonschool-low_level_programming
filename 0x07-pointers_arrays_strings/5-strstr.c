c46e16c57c2ed5fe1668195555f4f3d909e8c345
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-10-06 01:21:54 +0000

    Fix outputcheck

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -20,6 +20,11 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
+		if (count == nSize)
+		{
+			return (&haystack[index]);
+		}
+
 		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i] == needle[j + count])
