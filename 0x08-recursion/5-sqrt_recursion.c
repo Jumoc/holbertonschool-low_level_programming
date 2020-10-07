7a3b866573c30412a097cb857349adc3d08d8bd2
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-10-07 23:17:28 +0000

    Add C program to return the sqrt of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_internal -  returns the natural square root of a number
+ *
+ * @n: constant number to operate
+ * @aux: aux of n to iterate
+ *
+ * Return: the natural square root of n
+ */
+
+int _sqrt_internal(int n, int aux)
+{
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (aux < 0)
+	{
+		return (0);
+	}
+
+	if (n == 0 || aux == 0)
+	{
+		return (0);
+	}
+
+	if (aux * aux == n)
+	{
+		return (aux);
+	}
+
+	return (_sqrt_internal(n, aux - 1));
+}
+
+/**
+ * _sqrt_recursion -  returns the natural square root of a number
+ *
+ * @n: number to calculate its square root
+ *
+ * Return: the natural square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt_internal(n, n));
+}
