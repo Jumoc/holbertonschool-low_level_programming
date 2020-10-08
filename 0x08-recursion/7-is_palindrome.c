77f7ef57d4e216f3f6e6d632aacea3825f0abe72
Juan Pablo Montoya <juanpcaicedo2@gmail.com>
2020-10-08 02:49:08 +0000

    Fix betty check

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,3 +1,14 @@
+
+/**
+ * is_palindrome_internal - checks for palindromes
+ *
+ * @s: string to check
+ * @size: size of the string
+ * @count: count the string from zero
+ *
+ * Return: 1 if palindrome 0 otherwise
+ */
+
 int is_palindrome_internal(char *s, int size, int count)
 {
 	if (size >= 0)
@@ -12,9 +23,17 @@ int is_palindrome_internal(char *s, int size, int count)
 		return (1);
 	}
 
-	return(is_palindrome_internal(s, size - 1, count + 1));
+	return (is_palindrome_internal(s, size - 1, count + 1));
 
 }
+
+/**
+ *is_palindrome_size - checks the size of a string
+ *
+ * @s: string to be counted
+ *
+ * Return: size of
+ */
 
 int is_palindrome_size(char *s)
 {
@@ -27,6 +46,14 @@ int is_palindrome_size(char *s)
 	return (is_palindrome_size(s + 1) + 1);
 
 }
+
+/**
+ * is_palindrome - checks for palindromes
+ *
+ * @s: string to check
+ *
+ * Return: 1 if palindrome 0 otherwise
+ */
 
 int is_palindrome(char *s)
 {
