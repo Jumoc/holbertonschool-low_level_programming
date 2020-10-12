d1a402b81e8a9757988880f51ad45cec0d0cb0e7
Jumoc <juanpcaicedo2@gmail.com>
2020-10-12 01:43:13 -0500

    Add C program to print the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function 
+ * 
+ * @argc: amount of arguments
+ * @argv: array of arguments
+ * 
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
