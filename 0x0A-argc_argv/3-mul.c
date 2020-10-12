27fbd2ad7c7410dc47409ba844b0146928151cf1
Jumoc <juanpcaicedo2@gmail.com>
2020-10-12 02:13:18 -0500

    Add C program to multiply two passed arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * @argc: amount of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	
+    int operacion = 0;
+
+    if (argc == 3)
+    {
+        operacion = atoi(argv[1]) * atoi(argv[2]);
+        printf("%d\n", operacion);
+    } else {
+
+        printf("Error\n");
+        return (0);
+
+    }
+
+	return (0);
+}
