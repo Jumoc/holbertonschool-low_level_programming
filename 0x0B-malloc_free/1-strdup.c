001cac3677e58e60c429734477c773ab64fedb80
Jumoc <juanpcaicedo2@gmail.com>
2020-10-13 16:10:14 -0500

    Add C program that returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sizePtr - returns the size of a pointer array
+ *
+ * @str: pointer array to be counter
+ *
+ * Return: size of the array
+ */
+
+int sizePtr(char *str)
+{
+	int size;
+
+	for (size = 0; str[size] != '\0'; size++)
+		size++;
+	return (size);
+}
+
+/**
+ * _strdup - creates an array of size
+ *
+ * @str: size of the array
+ *
+ * Return: pointer to the new array
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int size, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = sizePtr(str);
+
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		ptr[i] = str[i];
+
+	return (ptr);
+}
