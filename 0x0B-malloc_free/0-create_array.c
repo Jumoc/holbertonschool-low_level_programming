9b603184e6f3dfc72d2b6ee30f4b0d903df798bb
Jumoc <juanpcaicedo2@gmail.com>
2020-10-13 15:33:42 -0500

    Add C program that creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of size
+ *
+ * @size: size of the array
+ * @c: char to initialize the array to
+ *
+ * Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	ptr = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+
+		ptr[i] = c;
+
+	}
+	return (ptr);
+}
