f9afc9d79f41367875d2e0860795dcf3466f28bb
Jumoc <juanpcaicedo2@gmail.com>
2020-10-13 23:23:57 -0500

    Fix program output

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 
 	matrix = (int **) malloc(height * sizeof(int *));
 
-	if(matrix == NULL)
+	if (matrix == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
