55a2e0369ae381b21f2283a7850c1d4a80693375
Jumoc <juanpcaicedo2@gmail.com>
2020-10-14 01:24:27 -0500

    Add C program that concatenates all the arguments of a program

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ *
+ * @ac: amount of args
+ * @av: args
+ *
+ * Return: pointer to the new 2d array
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int i, j, aux = 0, count = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			count++;
+			j++;
+		}
+	}
+
+	ptr = malloc((count + ac) * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			ptr[aux] = av[i][j];
+			aux++;
+			j++;
+		}
+		ptr[aux] = '\n';
+		aux++;
+	}
+	return (ptr);
+}
