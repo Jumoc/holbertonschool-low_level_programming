559256faac343bd6e4c46b9e55dce1021f8c39fc
Jumoc <juanpcaicedo2@gmail.com>
2020-10-19 17:46:53 -0500

    Fix betty check

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+ * struct dog - dog caracteristics
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ */
+struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 #endif
