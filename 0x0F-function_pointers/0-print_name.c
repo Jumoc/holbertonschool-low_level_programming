91d7bd3a44eb1716e6baa53d38c27402a02f1401
Jumoc <juanpcaicedo2@gmail.com>
2020-10-21 18:03:07 -0500

    Fix output and betty check

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,16 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name using another function.
+ *
+ * @name: name to be printed
+ * @f: function that prints the name
  *
  * Return: void.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
