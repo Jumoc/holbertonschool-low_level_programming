66bcc7959c986d7f4f387587e45a987114cc24a9
Jumoc <juanpcaicedo2@gmail.com>
2020-10-21 23:35:55 -0500

    Add program that creates the simple operation functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+/**
+ * op_add - operates two numbers
+ *
+ * @a: first numbers
+ * @b: second number
+ *
+ * Return: result of the operation.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - operates two numbers
+ *
+ * @a: first numbers
+ * @b: second number
+ *
+ * Return: result of the operation.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - operates two numbers
+ *
+ * @a: first numbers
+ * @b: second number
+ *
+ * Return: result of the operation.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - operates two numbers
+ *
+ * @a: first numbers
+ * @b: second number
+ *
+ * Return: result of the operation.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - operates two numbers
+ *
+ * @a: first numbers
+ * @b: second number
+ *
+ * Return: result of the operation.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
