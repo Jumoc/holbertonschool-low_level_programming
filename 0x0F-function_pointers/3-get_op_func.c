f5c292d828434f10311c82a2e0210f766f7358e9
Jumoc <juanpcaicedo2@gmail.com>
2020-10-21 23:35:05 -0500

    Add function that checks for a function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - operates two numbers
+ *
+ * @a: first numbers
+ * @b: second number
+ *
+ * Return: result of the operation.
+ */
+int (*get_op_func(char *s))(int a, int b)
+{
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (s != NULL && *(ops[i].op) == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
