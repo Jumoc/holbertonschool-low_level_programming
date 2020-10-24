e6af3975ebe9dcc6ced0942b86b3cdc33f239361
Jumoc <juanpcaicedo2@gmail.com>
2020-10-24 17:17:33 -0500

    Fix output

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,27 +5,24 @@
  * print_numbers - check the code for Holberton School students.
  *
  * @n: size of args
+ * @separator: separator of the numbers
  *
  * Return: Always 0.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
 	unsigned int i;
-
-	if (n == 0)
-                return;
+	va_list args;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-                if (separator != NULL && i != n - 1)
-                        printf("%d%s", va_arg(args, int), separator);
-                else
-                       printf("%d", va_arg(args, int)); 
+		if (separator != NULL && i != (n - 1))
+			printf("%d%s", va_arg(args, int), separator);
+		else
+		       printf("%d", va_arg(args, int));
 	}
-        printf("\n");
-
+	printf("\n");
 	va_end(args);
 }
