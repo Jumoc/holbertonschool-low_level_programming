f7ead59952fda23b2123379ec06afaed9b4408df
Jumoc <juanpcaicedo2@gmail.com>
2020-10-30 22:26:17 -0500

    Fix betty check

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-void printBefore (void) __attribute__ ((constructor)); 
+void printBefore(void) __attribute__ ((constructor));
 
+/**
+ * printBefore - prints a string before the main function
+ *
+ * Return: void
+ */
 void printBefore(void)
-{ 
-	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
