9376790cd02a9367b6c052ed565ee6ff65228727
Jumoc <juanpcaicedo2@gmail.com>
2020-10-30 21:35:10 -0500

    Add C that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * list_len - returns the size of a list
+ *
+ * @h: header of the singly linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		i++;
+		current = current->next;
+	}
+	return (i);
+}
+
+/**
+ * add_node_end - adds a node to a list
+ *
+ * @head: header of the singly linked list
+ * @str: string to add to the node
+ *
+ * Return: the head
+ */
+void free_list(list_t *head)
+{
+        list_t *current = head;
+        size_t size, i, j;
+
+        size = list_len(head);
+
+        for (i = 0; i < size; i++)
+        {
+                current = head;
+                for (j = 0; j < (size - i); j++)
+                {
+                        current = current->next;
+                }
+                free(current);
+        }
+
+}
