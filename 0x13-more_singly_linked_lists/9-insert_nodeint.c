41c2e3f4758384157c2408f35b8cd387ad758f85
Jumoc <juanpcaicedo2@gmail.com>
2020-11-04 13:49:02 -0500

    Fix checker not compiling

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * listint_len - returns the size of a list
+ *
+ * @h: header of the singly linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		i++;
+		current = current->next;
+	}
+	return (i);
+}
+
+/**
  * insert_nodeint_at_index - pops the head of the list and returns its value
  *
  * @head: header of the singly linked list
