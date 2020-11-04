1cd40f72723ca320f6e67759f5ac8b831762f493
Jumoc <juanpcaicedo2@gmail.com>
2020-11-04 14:04:36 -0500

    Fix checker not compiling

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * _listint_len - returns the size of a list
+ *
+ * @h: header of the singly linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t _listint_len(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		i++;
+		current = current->next;
+	}
+	return (i);
+}
+
+/**
  * reverse_listint - reverses a list
  *
  * @head: header of the singly linked list
@@ -15,7 +36,7 @@ listint_t *reverse_listint(listint_t **head)
 		return (NULL);
 	}
 
-	if (listint_len(*head) == 1)
+	if (_listint_len(*head) == 1)
 	{
 		return (*head);
 	}
