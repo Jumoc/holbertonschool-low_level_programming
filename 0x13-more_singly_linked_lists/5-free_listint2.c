06ba85281530c6396b41f401056b1c83447901de
Jumoc <juanpcaicedo2@gmail.com>
2020-11-04 22:19:38 -0500

    Fix output check

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,51 @@
 #include "lists.h"
 
 /**
+ * listint_len - returns the size of a list
+ *
+ * @h: header of the singly linked list
+ *
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		i++;
+		current = current->next;
+	}
+	return (i);
+}
+
+/**
+ * free_listint - frees a singly linked list
+ *
+ * @head: header of the singly linked list
+ *
+ * Return: number of nodes
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+	int size, i, j;
+
+	size = listint_len(head);
+
+	for (i = 0; i < size; i++)
+	{
+		current = head;
+		for (j = 0; j < (size - i - 1); j++)
+			current = current->next;
+		free(current);
+	}
+}
+
+/**
  * free_listint2 - frees a singly linked list and sets the head to null
  *
  * @head: header of the singly linked list
@@ -9,14 +54,10 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *current;
-
-	current = *head;
-
-	while(*head != NULL)
+	if (head != NULL)
 	{
-		(*head) = (*head)->next;
-		free(current);
-		current = *head;
+		free_listint((*head));
+		head = NULL;
 	}
+	*head = NULL;
 }
