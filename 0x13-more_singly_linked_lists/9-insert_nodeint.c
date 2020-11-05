29e46acaf83a776a3addaba6a781fa1abf181607
Jumoc <juanpcaicedo2@gmail.com>
2020-11-04 20:25:59 -0500

    Fix missing output checks

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,32 @@
 #include "lists.h"
 
 /**
+ * add_nodeint - adds an int node to a list
+ *
+ * @head: header of the singly linked list
+ * @n: data of the node
+ *
+ * Return: added node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode;
+
+	newNode = malloc(sizeof(listint_t));
+
+	if (!newNode)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = (*head);
+	*head = newNode;
+
+	return (*head);
+
+}
+
+/**
  * _listint_len - returns the size of a list
  *
  * @h: header of the singly linked list
@@ -49,6 +75,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	newNode->n = n;
+
+	if ((*head) == NULL)
+	{
+		(*head) = newNode;
+		newNode->next = NULL;
+		return (newNode);
+	}
+
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		(*head) = newNode;
+		return (*head);
+	}
 
 	while (i < (idx - 1))
 	{
