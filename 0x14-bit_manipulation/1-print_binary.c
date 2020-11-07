3e186e159f407ef79d260a17338b047ab104fb4d
Jumoc <juanpcaicedo2@gmail.com>
2020-11-06 21:43:05 -0500

    Print binary with bitwise manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints a decimal number to binary;
+ * @n: number to convert
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i, k;
+
+	if (!n)
+		_putchar('0');
+
+	for (i = 63; i >= 0; i--)
+	{
+		k = n >> i;
+		if (k)
+		{
+			if (k & 1)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+	}
+	_putchar('\n');
+}
