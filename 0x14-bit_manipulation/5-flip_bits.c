31c77d92e2d8b12cdacd6547f22eafdd1e844674
Jumoc <juanpcaicedo2@gmail.com>
2020-11-06 23:48:37 -0500

    Add C function to count the amount of bits it would taka to go from one number to another

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - gets the nth bit of a number
+ * @n: number to convert
+ * @m: bit to get
+ * Return: 1 or 0
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int base, aux;
+	unsigned int count = 0;
+	int i;
+
+	base = n ^ m;
+	for (i = ((sizeof(unsigned long int) * 8) - 1); i >= 0; i--)
+	{
+		
+		aux = base >> i;
+
+		if (aux)
+		{
+			if (aux & 1)
+			{
+				count++;
+			}
+		}
+	}
+	return (count);
+}
