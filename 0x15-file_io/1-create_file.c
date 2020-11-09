52a5bf729ef34c6bcd25f045e0cded1b8b29a892
Jumoc <juanpcaicedo2@gmail.com>
2020-11-09 17:51:13 -0500

    Fix code try

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,7 +39,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		size = _strlen(text_content);
-		write(fd, text_content, size);
+		if (write(fd, text_content, size) < size)
+			return (-1);
 	}
 	close(fd);
 	return (1);
