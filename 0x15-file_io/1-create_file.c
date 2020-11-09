5fcf9d92a6878f224fadc5e96efeb19b16ebfe46
Jumoc <juanpcaicedo2@gmail.com>
2020-11-09 17:49:46 -0500

    Add C function to create a file and put stuff in it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ * _strlen - function that obtain the length of buffer
+ * @buffer: pointer contain digits
+ * Return: size of buffer
+ */
+int _strlen(char *buffer)
+{
+	int size = 0;
+
+	while (buffer[size] != '\0')
+	{
+		size++;
+	}
+	return (size);
+}
+
+/**
+ * create_file - creates a files and puts text in it
+ * @filename: name of the file
+ * @text_content: text that the file is going to contain
+ *
+ * Return: amount of characters printed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int size;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		size = _strlen(text_content);
+		write(fd, text_content, size);
+	}
+	close(fd);
+	return (1);
+}
