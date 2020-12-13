554632fb2a439457f83e1e83967709a7416bdd59
Jumoc <juanpcaicedo2@gmail.com>
2020-12-13 02:04:55 -0500

    Fix output check

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,6 +19,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	newNode->prev = NULL;
 
 	newNode->next = *head;
+	if (*head)
+		(*head)->prev = newNode;
 	*head = newNode;
 	return (*head);
 }
