c45ec800f2bfe4630b1d3bf96909ba9d214deb5d
Jumoc <juanpcaicedo2@gmail.com>
2021-01-15 01:40:56 -0500

    Fix betty check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,12 +18,12 @@ hash_node_t *create_node(const char *key, const char *value)
 	if (!newNode)
 		return (NULL);
 
-    newNode->key = malloc(strlen(key) + 1 * sizeof(char));
+	newNode->key = malloc(strlen(key) + 1 * sizeof(char));
 	newNode->value = malloc(strlen(value) + 1 * sizeof(char));
 
 	strcpy(newNode->key, key);
 	strcpy(newNode->value, value);
-    newNode->next = NULL;
+	newNode->next = NULL;
 
 	return (newNode);
 }
@@ -31,7 +31,8 @@ hash_node_t *create_node(const char *key, const char *value)
 /**
  * add_node - adds an int node to a list
  *
- * @node: header of the singly linked list
+ * @node: node to add
+ * @head: heade of the singly linked
  *
  * Return: added node
  */
@@ -53,21 +54,21 @@ hash_node_t *add_node(hash_node_t **head, hash_node_t *node)
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int idx;
-    hash_node_t *node = NULL;
+	unsigned long int idx;
+	hash_node_t *node = NULL;
 
-    if (!key || !ht || strcmp(value, "") == 0 || strcmp(key, "") == 0)
-        return (0);
+	if (!key || !ht || strcmp(value, "") == 0 || strcmp(key, "") == 0)
+		return (0);
 
-    idx = key_index((unsigned char *) key, ht->size);
-    node = create_node(key, value);
-    if (!node)
-        return (0);
+	idx = key_index((unsigned char *) key, ht->size);
+	node = create_node(key, value);
+	if (!node)
+		return (0);
 
-    if (!(ht->array[idx]))
-        ht->array[idx] = node;
-    else
-        add_node(&(ht->array[idx]), node);
+	if (!(ht->array[idx]))
+		ht->array[idx] = node;
+	else
+		add_node(&(ht->array[idx]), node);
 
-    return (1);
+	return (1);
 }
